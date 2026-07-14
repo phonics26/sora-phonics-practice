ee2204b61b06db5386fa793b05612e918286a4f1
phonics26 <purple@sora-academy.jp>
2026-07-14 18:38:12 +0900

    test

--- src/router.js
+++ src/router.js
@@ -1,11 +1,3 @@
-import { renderHomePage } from './pages/HomePage.js'
-import { renderActivity1 } from './pages/Activity1.js'
-
-const pages = {
-  home: renderHomePage,
-  activity1: renderActivity1,
-}
-
 export function navigate(pageName) {
   const page = pages[pageName]
 
